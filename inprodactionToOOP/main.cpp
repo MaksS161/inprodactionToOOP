34274a60625752109688af1ff534700cb2f473de
Admin <Admin@ADMIN>
2023-04-08 13:22:57 +0300

    encoding

--- inprodactionToOOP/main.cpp
+++ inprodactionToOOP/main.cpp
@@ -6,19 +6,63 @@ class Point
 	double x;
 	double y;
 public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this -> x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
 
+	//		Constructors:
+	Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "1ArgConstructor:\t" << this << endl;
+
+	}
+	Point(double x,double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//		Metodds:
+	void print()const
+	{
+		cout << "X= " << x<< "\tY = " << y << endl;
+	}
 }; 
 //#define STRUC_POINT
 void main()
 {
 	setlocale(LC_ALL, "");
 #ifdef STRUC_POINT
-
-
 	int a;//объявление переменной 'a' типа 'int'
 	Point A;//обявление переменной 'А' типа 'Point'
-			// Объявление объекта А' структуры 'Point'
-			// Создание экземпляра 'А' структуры 'Point'
+	// Объявление объекта А' структуры 'Point'
+	// Создание экземпляра 'А' структуры 'Point'
 	A.x = 2;
 	A.y = 3;
 	cout << A.x << "\t" << A.y << endl;
@@ -26,4 +70,16 @@ void main()
 	Point* pA = &A;
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUC_POINT
+
+	Point A;
+	//A.set_x(2);
+	//A.set_y(3);
+	//cout << A.get_x() << "\t" << A.get_y() << endl;
+	A.print();
+
+	Point B = 5;
+	B.print();
+
+	Point C(2, 3);
+	C.print();
 }
