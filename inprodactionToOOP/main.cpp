766ff14f96ef68391f37ceb58d5b53e3870e3d14
Admin <Admin@ADMIN>
2023-04-08 11:42:51 +0300

    encoding

--- inprodactionToOOP/main.cpp
+++ inprodactionToOOP/main.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
 using namespace std;
 
+class Point
+{
+	double x;
+	double y;
+public:
+
+}; 
+//#define STRUC_POINT
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Привет" << endl;
+#ifdef STRUC_POINT
+
+
+	int a;//объявление переменной 'a' типа 'int'
+	Point A;//обявление переменной 'А' типа 'Point'
+			// Объявление объекта А' структуры 'Point'
+			// Создание экземпляра 'А' структуры 'Point'
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+	cout << sizeof(A) << endl;
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRUC_POINT
 }
