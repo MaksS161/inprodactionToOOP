2a62ce72363d7caedcc8311a1a0dc40dd7cd52b6
Admin <Admin@ADMIN>
2023-05-22 20:50:41 +0300

    encoding

--- String!/main.cpp
+++ String!/main.cpp
@@ -1,14 +1,144 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
+
+class String;
+String operator+(const String& left, const String& right);
+
 
 class String
 {
-	int size;	//размер строки
-	char* str;	// адрес строки в динамической памяти
+	int size;		//размер строки в Байтах
+	char* str;		//адрес строки в динамической памяти
+public:
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str()const
+	{
+		return this->str;
+	}
+	char* get_str()
+	{
+		return this->str;
+	}
+
+	//				Constructors:
+	explicit String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "Constructor:\t" << this << endl;
+	}
+	String(const char str[])
+	{
+		this->size = strlen(str) + 1; //Послкольку класс хранит размер в байтаз, +1 нужен для хранения NULL элемента
+		this->str = new char[size] {};//выделяем память подстраку 
+		for (int i = 0; i<size; i++)this->str[i] = str[i];
+		cout << "Constructor:\t" << this << endl;
+	}
+	String(const String& other)
+	{
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	String(String&& other)
+	{
+		this->size = other.size;
+		this->str = other.str;
+		other.str = nullptr;
+		cout << "MoveConstructor:" << this << endl;
+	}
+	~String()
+	{
+		delete[] this->str;
+		cout << "Destructor:\t" << this << endl;
+	}
+	//			Operators:
+	String& operator=(const String& other)
+	{
+	
+		//if (this == &other)return *this;
+		delete[] this->str;
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)
+			this->str[i] = other.str[i];
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+	String& operator+=(const String& other)
+	{
+		return *this = *this + other;
+	}
+	char operator[](int i) const
+	{
+		return str[i];
+	}
+	char& operator[](int i)
+	{
+		return str[i];
+	}
+
+	//				Methods:
+	void print()const
+	{
+		cout << "Size: " << size << endl;
+		cout << "Str:  " << str << endl;
+	}
 };
+
+String operator+(const String& left, const String& right)
+{
+	String cat  (left.get_size() + right.get_size()-1);
+	for (int i = 0; i < left.get_size(); i++)
+	{
+	//	cat.get_str()[i] = left.get_str()[i];
+		cat[i] = left[i];
+	}
+	for (int i = 0; i < right.get_size(); i++)
+	{
+	//	cat.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+		cat[i + left.get_size() - 1] = right[i];
+	}
+	return cat;
+}
+
+std::ostream& operator<<(std::ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+
+#define HOME_WORK
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hello" << endl;
+	/*cout << "Hello" << endl;
+	String str1;
+	str1.print();*/
+
+	//String str = "Hello";
+	////str.print();
+	//cout << str << endl;
+
+#ifdef HOME_WORK
+	String str1 = "Hello";
+	cout << str1 << endl;
+
+	String str2 = "World";
+	cout << str2 << endl;
+	
+	//String str3 = str1 + str2;
+	String str3;
+	//str3 = str1 + str2;
+
+	cout << str3 << endl;
+	str1 += str2;
+	cout << str1 << endl;
+
+#endif // HOME_WORK
+
 }
